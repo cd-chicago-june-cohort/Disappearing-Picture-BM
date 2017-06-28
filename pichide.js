d4c8830758295f5c6c68db3e33c9cb924e03b47d
Bald Mike <baldmike312@gmail.com>
2017-06-28 14:47:16 -0500

    initial commit

---
+++ pichide.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+
+
+
+
+$('.picture').click(function(){
+    $(this).hide('slow');
+    console.log("help");
+})
+
+$('#restore').click(function() {
+    location.reload();
+})
+
+
+
+
+
+});
